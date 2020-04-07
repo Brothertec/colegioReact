e3822d211aa42d4cf342a7a1cb5813a500362e8a
Romulo Gomes <romulo.redg@gmail.com>
2020-04-07 13:48:16 -0300

    Commit inicial

---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Nav from '../Nav/Nav';
+
+import Carosel from '../Carosel/Carosel';
+
+export default function Home() {
+    return (
+        <div>
+            <Nav/>
+            <Carosel/>
+        </div>
+        
+    );
+}
